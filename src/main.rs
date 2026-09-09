7bed2e42ec74a36b5cfafa1a81dd5130e95f791f
1nuc <ahmedalyafai85@gmail.com>
2026-09-09 18:33:55 +0800

    Apply changes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    error::Error, io::{BufRead, BufReader, BufWriter, Read, Write}, net::{TcpListener, TcpStream},
+    error::Error, fmt::format, io::{BufRead, BufReader, BufWriter, Read, Write}, net::{TcpListener, TcpStream},
 };
 
 fn main() {
@@ -8,18 +8,26 @@ fn main() {
     for stream in server.incoming() {
         match stream{
             Ok(str) =>{
-                handle_request(str);},
+                let req_body=handle_request(&str);
+                handle_response(str);
+                println!("{req_body:#?}")
+            },
             Err(err) => eprint!("{err:?}"),
         }
     }
     println!("Hello, world!");
 }
 
-fn handle_request(stream: TcpStream) -> Vec<String>{
+fn handle_request(stream: &TcpStream) -> Vec<String>{
     let buffer = BufReader::new(stream);
     buffer
         .lines()
         .map(|x| x.unwrap())
-        .take_while(|x| x.is_empty())
+        .take_while(|x| !x.is_empty())
         .collect::<Vec<_>>()
+}
+fn handle_response(mut stream: TcpStream){
+    //this is the singature for the get request response HTTP/1.1 200 Ok \r\n\r\n
+    let res="HTTP/1.1 200 Ok \r\n\r\n the message is received now";
+    stream.write_all(res.as_bytes()).expect("error in right the response");
 }
