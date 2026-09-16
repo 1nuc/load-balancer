406f65fc45c16e2b3b72490b80347aa1ff27ef4e
1nuc <ahmedalyafai85@gmail.com>
2026-09-16 15:52:11 +0800

    Apply changes

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,42 @@
-use std::{net::SocketAddr};
+use std::{net::SocketAddr, time::Duration};
 
 use http_body_util::Full;
 use hyper::{Request, Response, body, server::conn::http2, service::service_fn, body::Bytes};
 use hyper_util::rt::{TokioExecutor, TokioIo};
-use tokio::net::TcpListener;
+use tokio::{net::TcpListener, time::sleep};
 
 async fn handle(req: Request<body::Incoming>) -> Result<Response<http_body_util::Full<Bytes>>, hyper::Error>{
-    let req =req.into_body();
-    println!("the sent request is {req:?}");
+    let _req =req.into_body();
     let http_response= Response::new(Full::new(Bytes::from("hi there".to_string())));
     Ok(http_response)
 }
+#[allow(unused_must_use)]
 #[tokio::main]
 async fn main() {
     //assinging the ip address
     let ip=SocketAddr::from(([127,0,0,1], 3000));
     //starting the server
     let server=TcpListener::bind(ip).await.expect("unable to bind the ip");
-
     loop {
-        match server.accept().await{
-            Ok((stream, _ip))=>{
-                let http2=http2::Builder::new(TokioExecutor::new());
-                let io=TokioIo::new(stream);
-                http2.serve_connection(io, service_fn(handle));
-            },
-            Err(err) => {
-                eprint!("{err:?}");
-            },
+        if let Ok((stream, _))=server.accept().await{
+            let http2= http2::Builder::new(TokioExecutor::new());
+            let io=TokioIo::new(stream);
+            http2.serve_connection(io, service_fn(handle));
         }
     }
+    // loop {
+    //     match server.accept().await{
+    //         Ok((stream, _ip))=>{
+    //             let http2=http2::Builder::new(TokioExecutor::new());
+    //             println!("hello");
+    //             let io=TokioIo::new(stream);
+    //             println!("hello");
+    //             sleep(Duration::from_secs(4));
+    //             http2.serve_connection(io, service_fn(handle));
+    //         },
+    //         Err(err) => {
+    //             eprint!("{err:?}");
+    //         },
+    //     }
+    // }
 }
